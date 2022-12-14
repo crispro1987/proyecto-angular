4326b065af857a5167b444432971d71231374179
Cristian Vargas <77356085+crispro1987@users.noreply.github.com>
2022-12-14 20:37:33 -0300

    footer ok

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarModule } from './shared/navbar/navbar.module';
@@ -16,6 +16,8 @@ import { MaterialModule } from './material.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
     NavbarModule,
     FooterModule,
     FrontModule,
--- client/src/app/shared/footer/footer.module.ts
+++ client/src/app/shared/footer/footer.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer.component';
-
-
+import { MaterialModule } from 'src/app/material.module';
 
 @NgModule({
   declarations: [FooterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule
   ],
   exports: [FooterComponent]
 })

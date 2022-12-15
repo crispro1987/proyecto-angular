bc3c4d0e1b6e215a9c802f67cca956622a74611b
Cristian Vargas <77356085+crispro1987@users.noreply.github.com>
2022-12-15 20:17:28 -0300

    test unitarios de servicio y componente ok

--- client/src/app/front/front.component.spec.ts
+++ client/src/app/front/front.component.spec.ts
@@ -1,25 +1,45 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ApiService } from '../shared/services/api.service';
+import { FormBuilder } from '@angular/forms';
 import { FrontComponent } from './front.component';
+import { ProductsPipe } from './shared/filters/products.pipe';
+
 
 describe('FrontComponent', () => {
   let component: FrontComponent;
   let fixture: ComponentFixture<FrontComponent>;
+  let service: ApiService;
+  let httpTestingController : HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FrontComponent ]
+      declarations: [ FrontComponent, ProductsPipe ],
+      imports: [HttpClientTestingModule],
+      providers: [FormBuilder]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
+    service = TestBed.inject(ApiService);
+    httpTestingController = TestBed.inject(HttpTestingController);
     fixture = TestBed.createComponent(FrontComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('debe dar cantidad de páginas de 8 productos', () => {
+    component.pages = 2;
+    // Definimos la lista de cursos y el número de páginas esperado
+    component.courses = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    const expected = 2;
+
+    // Llamamos a la propiedad btnPaginate para que calcule el número de páginas
+    component.btnPaginate();
+
+    // Verificamos que el número de páginas coincide con lo esperado
+    expect(component.pages).toEqual(expected);
   });
 });
--- client/src/app/shared/services/api.service.spec.ts
+++ client/src/app/shared/services/api.service.spec.ts
@@ -1,16 +1,27 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ApiService } from './api.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('ApiService', () => {
   let service: ApiService;
 
+  let httpTestingController : HttpTestingController;
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(ApiService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('Recuperar todos los datos de tabla', () => {
+    service.readAll('courses').subscribe(todos => {
+      console.log(todos);
+    });
+    const req = httpTestingController.expectOne('https://api-angular.dosiscl.com/courses');
+    expect(req.request.method).toBe('GET');
+    req.flush({ status: 200, total: 1, results: [{id_course: 1, title_course: 'titulo de curso', url_course: 'url-course', instructor_course: 'Instructor', short_course: 'descripcion corta', image_course: 'imagen', alt_course: 'alt imagen', value_course: 2, value_offer_course: 1, mesh_course: [{ texto: 'mesh'}], tag_course: 'tag de lenguajes', date_created_course: '2022-12-15', date_updated_course: ''}]})
   });
 });
